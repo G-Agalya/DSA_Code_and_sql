540dddb0319b84508d9d6f0e54952470f154156f
Agalya G <agalya242005@gmail.com>
2025-07-01 12:39:50 +0530

    Time: 11 ms (3.07%) | Memory: 45.4 MB (7.79%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,7 +1,8 @@
 class Solution {
     public int strStr(String h, String n) {
-        for(int i=0;i<h.length();i++){
-            for(int j=i;j<h.length();j++){
+        int len=h.length();
+        for(int i=0;i<len;i++){
+            for(int j=i;j<len;j++){
                 String sub=h.substring(i,j+1);
                 if(sub.equals(n)){
                     return i;
