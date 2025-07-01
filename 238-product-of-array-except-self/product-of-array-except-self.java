ca3db25e79bfc5e4fb070c1352f02d1bf300c734
Agalya G <agalya242005@gmail.com>
2025-07-01 15:21:53 +0530

    Time: 681 ms (3.89%) | Memory: 56.9 MB (6.00%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int len=nums.length;
+        int[] pre=new int[len];
+        int[] sif=new int[len];
+        int pro=1;
+        int po=1;
+       
+        for(int i=0;i<len;i++){
+             pre[i]=pro;
+            pro*=nums[i];
+           
+            System.out.print(pro+" ");
+        }
+        
+        System.out.println();
+        for(int i=len-1;i>=0;i--){
+            sif[i]=po;
+            po*=nums[i];
+            
+            System.out.print(po+" ");
+            
+        }
+        for(int i=0;i<nums.length;i++){
+            nums[i]=pre[i]*sif[i];
+        }
+        return nums;
+    }
+}
