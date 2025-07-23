9983a5269a86a7b0b3f122b8b4118487fd4a6ee6
Agalya G <agalya242005@gmail.com>
2025-07-23 15:56:10 +0530

    Time: 19 ms (0.99%) | Memory: 41.2 MB (8.37%) - LeetSync

---
+++ 1769-get-maximum-in-generated-array/get-maximum-in-generated-array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int getMaximumGenerated(int n) {
+        int[] arr=new int[n+1];
+        if(n==0){
+            return 0;
+        }
+        arr[0]=0;
+        arr[1]=1;
+        for(int i=1;i<n+1;i++){
+            if((2*i)+1>n){
+              break;
+            }
+            else{
+                arr[2*i]=arr[i];
+                arr[2*i+1]=arr[i]+arr[i+1];
+            }
+        }
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<n+1;i++){
+            System.out.print(arr[i]+" ");
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        return max;
+    }
+}
