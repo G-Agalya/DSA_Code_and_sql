1f1ce8d512e6b8d7290e5a57060e081609566c49
Agalya G <agalya242005@gmail.com>
2025-07-25 22:55:48 +0530

    Time: 1 ms (98.61%) | Memory: 42.5 MB (14.88%) - LeetSync

--- 3396-valid-word/valid-word.java
+++ 3396-valid-word/valid-word.java
@@ -1,24 +1,26 @@
 class Solution {
     public boolean isValid(String word) {
-        int v = 0;
-        int c1 = 0;
-
-        if (word.length() < 3) {
-            return false;
-        }
-
-        for (char c : word.toCharArray()) {
-            if (!Character.isLetterOrDigit(c)) {
-                return false; // contains invalid character
-            }
-
-            if ("aeiouAEIOU".indexOf(c) >= 0) {
+        int c=0;
+        int v=0;
+        int co=0;
+        for(int i=0;i<word.length();i++){
+            char w=word.charAt(i);
+            if((w=='a' || w=='e' || w=='i' || w=='o' || w=='u') ||(w=='A' || w=='E' || w=='I' || w=='O' || w=='U')){
                 v++;
-            } else if (Character.isLetter(c)) {
-                c1++;
+            }
+            else if((w>='a' && w<='z') || (w>='A' && w<='Z')){
+                co++;
+            }
+            if((w>='a' && w<='z') || (w>='A' && w<='Z') ||(w>='0' && w<='9')){
+                c++;
+            }
+            else{
+                return false;
             }
         }
-
-        return v >= 1 && c1 >= 1;
+        if(word.length()>=3 && v>=1 && co>=1){
+            return true;
+        }
+        return false;
     }
 }
