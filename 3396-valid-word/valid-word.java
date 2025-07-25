0fbb694784443a28b1e92c3413fe3cb4a483d7ab
Agalya G <agalya242005@gmail.com>
2025-07-25 22:58:32 +0530

    Time: 1 ms (98.61%) | Memory: 42 MB (59.84%) - LeetSync

--- 3396-valid-word/valid-word.java
+++ 3396-valid-word/valid-word.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean isValid(String word) {
-        int c=0;
         int v=0;
         int co=0;
         for(int i=0;i<word.length();i++){
             char w=word.charAt(i);
+            if(!((w>='a' && w<='z') || (w>='A' && w<='Z') ||(w>='0' && w<='9'))){
+                return false;
+            }
             if((w=='a' || w=='e' || w=='i' || w=='o' || w=='u') ||(w=='A' || w=='E' || w=='I' || w=='O' || w=='U')){
                 v++;
             }
             else if((w>='a' && w<='z') || (w>='A' && w<='Z')){
                 co++;
             }
-            if(!((w>='a' && w<='z') || (w>='A' && w<='Z') ||(w>='0' && w<='9'))){
-                return false;
-            }
+        
         }
         if(word.length()>=3 && v>=1 && co>=1){
             return true;
