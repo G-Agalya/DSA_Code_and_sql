d306288079073f40d5bbcee8d56a83da3148f919
Agalya G <agalya242005@gmail.com>
2025-08-03 17:09:34 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (58.73%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -1,27 +1,10 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        List<Integer> sum1=new ArrayList<>();
-        List<Integer> sum2=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            if(i<n){
-                sum1.add(nums[i]);
-            }
-            else{
-                sum2.add(nums[i]);
-            }
+        int[] res=new int[2*n];
+        for(int i=0;i<n;i++){
+            res[2*i]=nums[i];
+            res[(2*i)+1]=nums[i+n];
         }
-        int k=0;
-        int m=0;
-        for(int i=0;i<nums.length;i++){
-            if(i%2==0){
-                nums[i]=sum1.get(k);
-                k++;
-            }
-            else{
-                nums[i]=sum2.get(m);
-                m++;
-            }
-        }
-        return nums;
+        return res;
     }
 }
