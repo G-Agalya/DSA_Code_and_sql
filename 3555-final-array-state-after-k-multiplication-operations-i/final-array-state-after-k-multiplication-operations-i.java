23396b91930bfe647d9bee930999cfe9151e5bd3
Agalya G <agalya242005@gmail.com>
2026-01-09 14:33:24 +0530

    Time: 1 ms (100.00%) | Memory: 47 MB (7.37%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] getFinalState(int[] nums, int k, int m) {
         
         while(k!=0){
-            int y=Integer.MAX_VALUE;
+            int y=nums[0];
             int j=0;
             for(int i=0;i<nums.length;i++){
                 if(y>nums[i]){
