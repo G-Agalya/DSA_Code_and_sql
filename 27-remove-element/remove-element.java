1a6edbb51aaf89d7b62db4e0c1c458c291d604a3
Agalya G <agalya242005@gmail.com>
2026-04-08 23:47:32 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (14.38%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,16 +1,12 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int n=Integer.MAX_VALUE;
-        int c=0;
+        int y=0;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==val){
-                nums[i]=n;
-            }
-            else{
-                c++;
+            if(nums[i]!=val){
+                nums[y]=nums[i];
+                y++;
             }
         }
-        Arrays.sort(nums);
-        return c;
+        return y;
     }
 }
